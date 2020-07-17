fbe8421a038a600106051b2d54264088e654cb38
nuvallo <nuvallo.inc@gmail.com>
2020-07-17 19:50:59 -0400

    register endpoint created

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,15 +1,17 @@
 const supertest = require("supertest");
 const server = require("../api/server");
+const db = require("../database/dbConfig");
+
+afterAll(async () => {
+  await db.destroy();
+});
 
 describe("Authenication integration tests", () => {
-  it("POST //api/auth/register", async () => {
-    const res = await supertest(server).post("//api/auth/register").send({
-      username: "test",
-      password: "test",
-    });
+  it("POST /api/auth/register", async () => {
+    const res = await supertest(server)
+      .post("/api/auth/register")
+      .send({ username: "test", password: "test" });
     expect(res.statusCode).toBe(201);
     expect(res.headers["content-type"]).toBe("application/json; charset=utf-8");
-    expect(res.body.id).toBeDefined();
-    expect(res.body.username).toBe("test");
   });
 });
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const db = require("../database/dbConfig");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    // if (username) {
+    //   res.status(401).json({ message: "Username already taken" });
+    // }
+
+    const newUser = {
+      username: username,
+      password: password,
+    };
+
+    const user = await db("users").insert(newUser);
+
+    res.status(201).json({ user });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
